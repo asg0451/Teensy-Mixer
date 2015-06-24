ab7786f6f7b279577fa7ddda67f883d2e09e70cd
Miles Frankel <miles.frankel@gmail.com>
2015-06-23 23:19:28 -0400

    fixed error in driver

--- Driver/Driver.ino
+++ Driver/Driver.ino
@@ -1,8 +1,7 @@
-
 const int readPin1 = 22;
 const int readPin2 = 20;
 const int readPin3 = 18;
-const int led1 = 12; 
+const int led1 = 12;
 const int led2 = 10;
 const int sw1 = 8;
 
@@ -25,22 +24,21 @@ void setup() {
 // the loop() methor runs over and over again,
 // as long as the board has power
 
-int val1, val2, val3;
+int val1, val2, val3, sw_status;
 
 void loop() {
 //  sw_status = digitalRead(sw1);
   val1= analogRead(8); //A0 = pin 14
   val2= analogRead(6); //A1 = pin 15
   val3= analogRead(4); //A2 = pin 16
-  
+
   Serial.print(val1);
   Serial.print(" ");
   Serial.print(val2);
   Serial.print(" ");
-  Serial.print(val3);
-  Serial.print(" ");
-  Serial.println(sw_status);
+  Serial.println(val3);
+  /* Serial.print(" "); */
+  /* Serial.println(sw_status); */
   delay(250);
 
 }
-
