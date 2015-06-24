043dbaccf01a6a0b61340de60e0975070711daf9
Miles Frankel <miles.frankel@gmail.com>
2015-06-23 23:17:43 -0400

    slight revisions

--- client.rb
+++ client.rb
@@ -1,26 +1,25 @@
 #!/usr/bin/env ruby
 
+##
+# client.rb -- the client program for my Teensy-based mixer thing
+# author: Miles Frankel (miles.frankel at gmail.com)
+##
+
 # require 'pry'
 require 'serialport'
 
 def inputs_readout  # TODO: check for failure
   inputs = `pactl list sink-inputs`
-  inp_list = (inputs.split /Sink Input/).map do |s|
+  inp_list = (inputs.split(/Sink Input/)).map do |s|
     s.lstrip.rstrip if s != ''
   end .compact
   inp_nums = inp_list.map do |s|
-    (s.match /^#([0-9]+)/).captures
+    s.match(/^#([0-9]+)/).captures
   end .flatten
   prog_name = inp_list.map do |s|
-    (s.match /application.process.binary = "([a-zA-Z0-9\-_]+)"/).captures
+    s.match(/application.process.binary = "([a-zA-Z0-9\-_]+)"/).captures
   end .flatten
   Hash[prog_name.zip(inp_nums)]
-end
-
-def read_serial
-  file = '/dev/ttyACM0'
-  line = %x(tail -f -n 1 #{file}).lstrip.rstrip
-  line.split(' ').map(&:to_f)
 end
 
 def update_stream_vol(num, vol)
@@ -49,14 +48,16 @@ end
 def main
   sp = setup_serial  # use default values
   while (line = sp.gets.chomp)
-    trap('INT') do # trap ctrl-c
+    trap('INT') do # trap ctrl-c and close connection gracefully
       sp.close
       puts 'Exiting...'
       exit
     end
     vols = line.split(' ').map(&:to_f)
     puts vols.to_s + "\n\n" # for debugging
+    # temporary; TODO: hash to store which slider goes to which program
     inputs_readout.each_with_index do |(k, _a), i|
+      # convert 10-bit num into percent
       volume = ((vols[i] / 1024) * 100).to_i
       update_prog_vol(k, volume)
     end
